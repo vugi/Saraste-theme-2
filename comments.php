4c5baabcea213513fc49b376c260c2702f9aeae7
Ville Vuorenmaa <villevuor@gmail.com>
2012-07-16 17:28:51 +0300

    Comment form

--- comments.php
+++ comments.php
@@ -9,32 +9,29 @@
 
 <?php if ( have_comments() ) : ?>
 	<h3 id="comments"><?php comments_number('Ei kommentteja', '1 kommentti', '% kommenttia'); ?></h3>
-	<?php wp_list_comments(
-						array(
-							'style' => 'div',
-							'type' => 'comment',
-							'avatar_size' => 50,
-							'callback' => 'saraste_comment'
-							)
-						);	?>
-
-<?php
-	/* If there are no comments and comments are closed, let's leave a little note, shall we?
-	 * But we don't want the note on pages or post types that do not support comments.
-	 */
-	elseif ( ! comments_open() && ! is_page() && post_type_supports( get_post_type(), 'comments' ) ) :
-?>
+	<?php wp_list_comments('type=comment&callback=saraste_comment'); ?>
+<?php elseif ( ! comments_open() && ! is_page() && post_type_supports( get_post_type(), 'comments' ) ) : ?>
 	<p class="alert">Kommentointi on suljettu.</p>
 <?php endif; ?>
 
-<?php comment_form(
-				array(
-					'author' => '<input type="text" id="author" name="author"><span class="help-inline">Nimi (pakollinen)</span>',
-					'email' => '<input type="text" id="email" name="email"><span class="help-inline">Sähköposti (pakollinen, ei julkaista)</span>',
-					'url' => '<input type="text" id="url" name="url"><span class="help-inline">Kotisivu</span>',
-					'comment_field' => '<textarea id="comment" name="comment"></textarea>',
-					'comment_notes_after' => ''
-					)
-				); 
-?>
+<?php
 
+
+$fields = array('author' => '<p><input type="text" id="author" name="author"><span class="help-inline">Nimi (pakollinen)</span></p>',
+				'email' => '<p><input type="text" id="email" name="email"><span class="help-inline">Sähköposti (pakollinen, ei julkaista)</span></p>',
+				'url' => '<p><input type="text" id="url" name="url"><span class="help-inline">Kotisivu</span></p>');
+
+$args = array(	'fields' => $fields,
+				'title_reply' => '',
+				'comment_field' => '<textarea id="comment" name="comment"></textarea>',
+				'comment_notes_after' => '',
+				'comment_notes_before' => ''
+			);
+					
+?>
+<?php if (comments_open() && !is_page() && post_type_supports( get_post_type(), 'comments' ) ) : ?>
+	<h3>Jätä kommentti</h3>
+	<div class="well">
+		<?php comment_form($args); ?>
+	</div>
+<?php endif; ?>
--- functions.php
+++ functions.php
@@ -1,24 +1,23 @@
 ﻿<?php
 
-add_filter('excerpt_length', 'custom_excerpt_length', 999);
-function custom_excerpt_length( $length ) {
+add_filter('excerpt_length', 'saraste_excerpt_length', 999);
+function saraste_excerpt_length( $length ) {
 	return 15;
 }
 
-add_filter('excerpt_more', 'new_excerpt_more');
-function new_excerpt_more( $more ) {
+add_filter('excerpt_more', 'saraste_excerpt_more');
+function saraste_excerpt_more( $more ) {
 	return '...';
 }
 
 function saraste_comment($comment, $args, $depth) {
 	$GLOBALS['comment'] = $comment;
-	extract($args, EXTR_SKIP);
 ?>
 	<div <?php comment_class('well'); ?> id="comment-<?php comment_ID() ?>">
-		<?php echo get_avatar($comment, $args['avatar_size']); ?>
+		<?php echo get_avatar($comment, 50); ?>
 		<p class="comment-author vcard"><strong><?php comment_author_link(); ?></strong><span style="float: right;"><?php edit_comment_link('Muokkaa'); ?></p>
 		<p class="comment-meta"><a href="#comment-<?php comment_ID() ?>"><?php comment_date('j.n.Y'); ?> klo <?php comment_time('h:i'); ?></a></p>
-		<p class="comment-body"><?php comment_text(); ?></p>
+		<div class="comment-body"><?php comment_text(); ?></div>
 	</div>
 
 <?php

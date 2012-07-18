5a6aeb702fc654398db80d12f5de7771a77abedb
Ville Vuorenmaa <villevuor@gmail.com>
2012-07-19 00:15:26 +0300

    Sidebar subpage nav

--- sidebar.php
+++ sidebar.php
@@ -1,6 +1,21 @@
 <?php $opt = get_option('saraste_theme_options');	?>
 
 <aside>
+	<?php
+		if(is_page()){
+			$subpages = wp_list_pages('title_li=&sort_column=menu_order&child_of=' . $post->ID . '&echo=0');
+			if(empty($subpages)){
+				$parent = $post->post_parent;
+			} else {
+				$parent = $post->ID;
+			}
+			$siblings = wp_list_pages('title_li=&sort_column=menu_order&child_of=' . $parent . '&echo=0&depth=1');
+			if(!empty($siblings) && !empty($parent)){
+				echo '<h2><a href="' . get_permalink($parent) . '">' . get_the_title($parent) . '</a></h2>';
+				echo '<ul>' . $siblings . '</ul>';
+			}
+		}
+	?>
 	<div class="featured">
 		<div>
 			<a href="#"><img src="<?php echo $opt["nosto1_kuva"]; ?>" alt=""></a>

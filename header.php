b1a26fbdb20057326f2d0989c0b05641f1ead87c
Ville Vuorenmaa <villevuor@gmail.com>
2012-07-18 12:35:38 +0300

    iPhone zoom fix

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 <head>
 	<title><?php wp_title('|', true, 'right'); ?></title>
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 	
 	<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory'); ?>/style.css" />
 	

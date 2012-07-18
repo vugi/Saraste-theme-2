fba7be9bd90fa010f95e71c4cd85a9b9423a2c2f
Ville Vuorenmaa <villevuor@gmail.com>
2012-07-19 00:27:03 +0300

    Widget support

--- functions.php
+++ functions.php
@@ -5,6 +5,8 @@ add_image_size('article-big', 580, 250);
 add_image_size('article-small', 280, 120);
 add_image_size('sidebar-thumb', 160, 100);
 
+register_sidebar(array('name' => 'Sivupalkki', 'id' => 'sivupalkki', 'before_widget' => '', 'after_widget' => '', 'before_title' => '<h2>', 'after_title' => '</h2>));
+
 add_filter('excerpt_length', 'saraste_excerpt_length', 999);
 function saraste_excerpt_length( $length ) {
 	return 10;
@@ -178,133 +180,6 @@ function bootstrap_setup(){
 			call_user_func_array(array(&$this, 'end_el'), $cb_args);
 		}
 	}
-}
-
-add_action( 'admin_init', 'theme_options_init' );
-add_action( 'admin_menu', 'theme_options_add_page' );
-
-/**
- * Init plugin options to white list our options
- */
-function theme_options_init(){
-	register_setting('saraste_options', 'saraste_theme_options', 'theme_options_validate' );
-}
-
-/**
- * Load up the menu page
- */
-function theme_options_add_page() {
-	add_theme_page('Sivupalkki', 'Sivupalkki', 'edit_theme_options', 'theme_options', 'theme_options_do_page' );
-}
-
-
-/**
- * Create the options page
- */
-function theme_options_do_page() {
-	global $select_options, $radio_options;
-
-	if ( ! isset( $_REQUEST['settings-updated'] ) )
-		$_REQUEST['settings-updated'] = false;
-
-	?>
-	<div class="wrap">
-		<?php screen_icon(); echo "<h2>Sivupalkki</h2>"; ?>
-
-		<?php if ( false !== $_REQUEST['settings-updated'] ) : ?>
-			<div class="updated fade"><p><strong>Asetukset tallennettu.</strong></p></div>
-		<?php endif; ?>
-
-		<form method="post" action="options.php">
-			<?php settings_fields('saraste_options'); ?>
-			<?php $options = get_option( 'saraste_theme_options' ); ?>
-
-			<table class="form-table">
-				<tr valign="top"><th scope="row">Nosto 1</th>
-					<td>
-						<input id="saraste_theme_options[nosto1_teksti]" class="regular-text" type="text" name="saraste_theme_options[nosto1_teksti]" value="<?php esc_attr_e($options['nosto1_teksti']); ?>" />
-						<label class="description" for="saraste_theme_options[nosto1_teksti]">Teksti</label>
-						<br/>
-						<input id="saraste_theme_options[nosto1_linkki]" class="regular-text" type="text" name="saraste_theme_options[nosto1_linkki]" value="<?php esc_attr_e($options['nosto1_linkki']); ?>" />
-						<label class="description" for="saraste_theme_options[nosto1_linkki]">Linkin osoite)</label>
-						<br/>
-						<input id="saraste_theme_options[nosto1_kuva]" class="regular-text" type="text" name="saraste_theme_options[nosto1_kuva]" value="<?php esc_attr_e($options['nosto1_kuva']); ?>" />
-						<label class="description" for="saraste_theme_options[nosto1_kuva]">Kuvan osoite)</label>
-					</td>
-				</tr>
-				<tr valign="top"><th scope="row">Nosto 2</th>
-					<td>
-						<input id="saraste_theme_options[nosto2_teksti]" class="regular-text" type="text" name="saraste_theme_options[nosto2_teksti]" value="<?php esc_attr_e($options['nosto2_teksti']); ?>" />
-						<label class="description" for="saraste_theme_options[nosto2_teksti]">Teksti</label>
-						<br/>
-						<input id="saraste_theme_options[nosto2_linkki]" class="regular-text" type="text" name="saraste_theme_options[nosto2_linkki]" value="<?php esc_attr_e($options['nosto2_linkki']); ?>" />
-						<label class="description" for="saraste_theme_options[nosto2_linkki]">Linkin osoite)</label>
-						<br/>
-						<input id="saraste_theme_options[nosto2_kuva]" class="regular-text" type="text" name="saraste_theme_options[nosto2_kuva]" value="<?php esc_attr_e($options['nosto2_kuva']); ?>" />
-						<label class="description" for="saraste_theme_options[nosto2_kuva]">Kuvan osoite)</label>
-					</td>
-				</tr>
-				<tr valign="top"><th scope="row">Blogi 1</th>
-					<td>
-						<input id="saraste_theme_options[blog1_teksti]" class="regular-text" type="text" name="saraste_theme_options[blogi1_teksti]" value="<?php esc_attr_e($options['blogi1_teksti']); ?>" />
-						<label class="description" for="saraste_theme_options[blogi1_teksti]">Teksti</label>
-						<br/>
-						<input id="saraste_theme_options[blogi1_linkki]" class="regular-text" type="text" name="saraste_theme_options[blogi1_linkki]" value="<?php esc_attr_e($options['blogi1_linkki']); ?>" />
-						<label class="description" for="saraste_theme_options[blogi1_linkki]">Linkin osoite)</label>
-						<br/>
-						<input id="saraste_theme_options[blogi1_kuva]" class="regular-text" type="text" name="saraste_theme_options[blogi1_kuva]" value="<?php esc_attr_e($options['blogi1_kuva']); ?>" />
-						<label class="description" for="saraste_theme_options[blogi1_kuva]">Kuvan osoite)</label>
-					</td>
-				</tr>
-				<tr valign="top"><th scope="row">Blogi 2</th>
-					<td>
-						<input id="saraste_theme_options[blogi2_teksti]" class="regular-text" type="text" name="saraste_theme_options[blogi2_teksti]" value="<?php esc_attr_e($options['blogi2_teksti']); ?>" />
-						<label class="description" for="saraste_theme_options[blogi2_teksti]">Teksti</label>
-						<br/>
-						<input id="saraste_theme_options[blogi2_linkki]" class="regular-text" type="text" name="saraste_theme_options[blogi2_linkki]" value="<?php esc_attr_e($options['blogi2_linkki']); ?>" />
-						<label class="description" for="saraste_theme_options[blogi2_linkki]">Linkin osoite)</label>
-						<br/>
-						<input id="saraste_theme_options[blogi2_kuva]" class="regular-text" type="text" name="saraste_theme_options[blogi2_kuva]" value="<?php esc_attr_e($options['blogi2_kuva']); ?>" />
-						<label class="description" for="saraste_theme_options[blogi2_kuva]">Kuvan osoite)</label>
-					</td>
-				</tr>
-			</table>
-			<p class="submit">
-				<input type="submit" class="button-primary" value="Tallenna" />
-			</p>
-		</form>
-	</div>
-	<?php
-}
-
-/**
- * Sanitize and validate input. Accepts an array, return a sanitized array.
- */
-function theme_options_validate( $input ) {
-	global $select_options, $radio_options;
-
-	// Our checkbox value is either 0 or 1
-	if ( ! isset( $input['option1'] ) )
-		$input['option1'] = null;
-	$input['option1'] = ( $input['option1'] == 1 ? 1 : 0 );
-
-	// Say our text option must be safe text with no HTML tags
-	$input['sometext'] = wp_filter_nohtml_kses( $input['sometext'] );
-
-	// Our select option must actually be in our array of select options
-	if ( ! array_key_exists( $input['selectinput'], $select_options ) )
-		$input['selectinput'] = null;
-
-	// Our radio option must actually be in our array of radio options
-	if ( ! isset( $input['radioinput'] ) )
-		$input['radioinput'] = null;
-	if ( ! array_key_exists( $input['radioinput'], $radio_options ) )
-		$input['radioinput'] = null;
-
-	// Say our textarea option must be safe text with the allowed tags for posts
-	$input['sometextarea'] = wp_filter_post_kses( $input['sometextarea'] );
-
-	return $input;
 }
 
 ?>
--- sidebar.php
+++ sidebar.php
@@ -16,31 +16,5 @@
 			}
 		}
 	?>
-	<div class="featured">
-		<div>
-			<a href="#"><img src="<?php echo $opt["nosto1_kuva"]; ?>" alt=""></a>
-			<a href="<?php echo $opt["nosto1_teksti"]; ?>"><h3><?php echo $opt["nosto1_teksti"]; ?></h3></a>
-		</div>
-		<div>
-			<a href="#"><img src="<?php echo $opt["nosto2_kuva"]; ?>" alt=""></a>
-			<a href="<?php echo $opt["nosto2_teksti"]; ?>"><h3><?php echo $opt["nosto2_teksti"]; ?></h3></a>
-		</div>
-	</div>
-	<h2>Live</h2>
-	<a href="#"><img src="http://placehold.it/340x225" alt=""></a>
-	<h2>Blogit</h2>
-	<div class="featured">
-		<div>
-			<a href="#"><img src="<?php echo $opt["blogi1_kuva"]; ?>" alt=""></a>
-			<a href="<?php echo $opt["blogi1_teksti"]; ?>"><h3><?php echo $opt["blogi1_teksti"]; ?></h3></a>
-		</div>
-		<div>
-			<a href="#"><img src="<?php echo $opt["blogi2_kuva"]; ?>" alt=""></a>
-			<a href="<?php echo $opt["blogi2_teksti"]; ?>"><h3><?php echo $opt["blogi2_teksti"]; ?></h3></a>
-		</div>
-	</div>
-	<div class="visible-desktop">
-		<h3>Facebook</h3>
-		<div class="fb-like-box" data-href="http://www.facebook.com/Saraste2012" data-width="340" data-height="400" data-show-faces="false" data-stream="true" data-header="false"></div>
-	</div>
+	<?php get_sidebar('sivupalkki'); ?>
 </aside>

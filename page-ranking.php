63657c74b66b9fdbee86c22e60fcd360776acde1
Ville Vuorenmaa <villevuor@gmail.com>
2012-07-27 00:10:42 +0300

    Purkki site

---
+++ page-ranking.php
@@ -0,0 +1,67 @@
+﻿<?php get_header(); ?>
+<article>
+<h2>Ranking</h2>
+<?php
+$query = "SELECT $wpdb->commentmeta.meta_value AS nimi, COUNT(DISTINCT $wpdb->comments.comment_post_ID) AS maara
+					FROM $wpdb->commentmeta, $wpdb->comments, $wpdb->postmeta
+					WHERE $wpdb->comments.comment_approved = '1'
+						AND $wpdb->commentmeta.comment_id = $wpdb->comments.comment_ID
+						AND $wpdb->commentmeta.meta_key = 'lippukunta'
+						AND $wpdb->postmeta.post_id = $wpdb->comments.comment_post_ID
+						AND $wpdb->postmeta.meta_key = 'Lippukunta'
+						AND $wpdb->postmeta.meta_value != $wpdb->commentmeta.meta_value
+					GROUP BY $wpdb->commentmeta.meta_value
+					ORDER BY maara DESC
+					LIMIT 10";
+					
+$lpkt = $wpdb->get_results($query);
+
+$i = $i2 = 1;
+
+if($lpkt){
+	echo '<h3>Eniten kätköjä löytäneet lippukunnat</h3><ol>';
+	foreach($lpkt as $lpk){
+		if($lpk->maara < $i2){
+			$i++;
+		}
+		echo '<li value="' . $i . '">' . $lpk->nimi . ' (' . $lpk->maara . ')</li>';
+		$i2 = $lpk->maara;
+	}
+	echo '</ol>';
+}
+
+$query = "SELECT purkki, ID, AVG(_avg) AS __avg
+					FROM
+					(
+						SELECT $wpdb->posts.post_title AS purkki, $wpdb->posts.ID, $wpdb->commentmeta.meta_value AS lpk, AVG(stars.meta_value) AS _avg
+						FROM $wpdb->posts, $wpdb->comments, $wpdb->commentmeta,
+						(
+							SELECT meta_value, comment_id 
+							FROM $wpdb->commentmeta
+							WHERE meta_key = 'arvio'
+						) AS stars
+						WHERE $wpdb->posts.ID = $wpdb->comments.comment_post_ID
+						AND $wpdb->comments.comment_id = $wpdb->commentmeta.comment_id
+						AND $wpdb->posts.post_type = 'purkit'
+						AND $wpdb->comments.comment_approved = 1
+						AND $wpdb->commentmeta.meta_key = 'lippukunta'
+						AND stars.comment_id = $wpdb->comments.comment_id
+						GROUP BY $wpdb->posts.post_title, $wpdb->commentmeta.meta_value
+					) AS purkit
+					GROUP BY purkki
+					ORDER BY __avg DESC
+					LIMIT 10";
+					
+$purkit = $wpdb->get_results($query);
+
+if($purkit){
+	echo '<h3>Parhaiten arvioidut kätköt</h3><ol>';
+	foreach($purkit as $p){
+		echo '<li><a href="' . get_permalink($p->ID) . '">' . $p->purkki . '</a></li>';
+	}
+	echo '</ol>';
+}
+
+?>
+</article>
+<?php get_footer(); ?>
---
+++ single-purkit.php
@@ -0,0 +1,55 @@
+﻿<?php get_header(); ?>
+
+<script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?sensor=false"></script>
+<script type="text/javascript">
+	function mapInit(){
+		var options = {
+			zoom: 10,
+			center: new google.maps.LatLng(60.23163, 24.908752),
+			mapTypeId: google.maps.MapTypeId.ROADMAP
+		}
+		var map = new google.maps.Map(document.getElementById("map_small"), options)
+		
+		var coord = jQuery(".hidden").text()
+		coord = coord.split(",")
+		
+		var marker = new google.maps.Marker({
+			position: new google.maps.LatLng(coord[0], coord[1]),
+			map: map,
+			title: jQuery("td:first-child", jQuery(this)).text(),
+			clickable: false,
+			icon: "<?php bloginfo('template_directory'); ?>/img/purkit/marker.png"
+		})
+	}
+	
+	jQuery(function(){
+		mapInit()
+	})
+</script>
+
+<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+				<article>
+					<h2><?php the_title(); ?></h2>
+					<p><?php	echo get_post_meta(get_the_ID(), 'Kuvaus', true);	?></p>
+					<?php
+						$fields = array("Lippukunta", "Vaikeusaste", "Ohjeet");
+						
+						foreach($fields as $field){
+							if(get_post_meta($post->ID, $field, true)){
+								$meta = get_post_meta(get_the_ID(), $field, true);
+								echo '<p class="meta ' . $field . '"><span>' . $field . '</span><span>' . ($field == "Vaikeusaste" ? purkit_taso($meta) : $meta) . '</span></p>';
+							}
+						}
+						
+						if(get_post_meta($post->ID, "Sijainti", true)){
+							echo '<p class="meta"><span>Sijainti</span>';
+							echo '<div id="map_container"><div id="map_small"></div></div></p>';
+							echo '<span class="hidden">' . get_post_meta(get_the_ID(), "Sijainti", true) . '</span><div style="clear: both;"></div>';
+						}
+						
+					?>
+					
+				<?php comments_template('/comments-purkit.php', true); ?>
+<?php endwhile; ?>
+
+<?php get_footer(); ?>

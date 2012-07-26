203611682ddbbe1fe5b6cc7e9192c09dd2afbbb0
Antti Vuorela <antti.vuorela@iki.fi>
2012-07-26 21:06:19 +0300

    Carousel dynamic loading of images one by one.

--- js/flickrCarousel.js
+++ js/flickrCarousel.js
@@ -5,7 +5,10 @@ $(function(){
     return;
   }
   
-  $container = $("#flickrCarousel .carousel-inner");
+  var $container = $("#flickrCarousel .carousel-inner");
+  var imgUrls = [];
+  var currentImage = -1;
+  var timeout = 5000;
   
   var parameters = { 
     api_key: "1f9dd955965f845c4828845466c99e02",
@@ -13,26 +16,46 @@ $(function(){
     method: "flickr.photosets.getPhotos",
     photoset_id:"72157630760953584"
   };
+  
+  function loadNextImage(){
+    loadImage(currentImage+1);
+  }
+  
+  function loadImage(index){
+    var url = imgUrls[index];
+    var img = new Image();
+    img.src = url;
+    img.onload = function(){
+      $container.append('<div class="item" style="background-image: url('+url+');"></div>');
+      $("#flickrCarousel").carousel('next');
+      currentImage = index;
+    }
+  }
 
   $.getJSON("http://api.flickr.com/services/rest/?jsoncallback=?",
     parameters,
     function(data){
       $(data.photoset.photo).each(function(i,item){
-        var url ="http://farm"+item.farm+".static.flickr.com/"+item.server+"/"+item.id+"_"+item.secret+"_b.jpg";
-        var itemClass = "item";
-        $container.append('<div class="'+itemClass+'" style="background-image: url('+url+');"></div>');
-        if(i==0){ 
-          var img = new Image();
-          img.src = url;
-          img.onload = function(){
-            $("#flickrCarousel").carousel('next');
-            $("#flickrCarousel .placeholder").remove();
-          }
-        }
+        var url = "http://farm"+item.farm+".static.flickr.com/"+item.server+"/"+item.id+"_"+item.secret+"_b.jpg";
+        imgUrls.push(url);
       });
-    $("#flickrCarousel").carousel({
-      interval: 5000
-    });
+      loadImage(0);
+      $("#flickrCarousel").carousel({
+        interval: timeout
+      }).carousel('pause'); 
   });
   
+  $("#flickrCarousel").on("slid",function(){
+    console.log("slid",currentImage)
+    if (currentImage == 0){
+      $("#flickrCarousel .placeholder").remove();
+    }
+    if (currentImage+1 < imgUrls.length){
+      setTimeout(loadNextImage,timeout);
+    } else if (currentImage+1 == imgUrls.length) {
+      console.log("start normal cycle");
+      $("#flickrCarousel").carousel('cycle');
+      currentImage++;
+    }
+  });
 });

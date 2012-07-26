912ef7a8b579b94490f17e69fa4d1edf6d3b64d3
Ville Vuorenmaa <villevuor@gmail.com>
2012-07-26 22:35:10 +0300

    Archive link fix

--- index.php
+++ index.php
@@ -3,6 +3,6 @@ get_header();
 
 query_posts('showposts=11');
 get_template_part('loop', 'index');
+
+get_footer();
 ?>
-<ul class="pager"><li><a href="<?php echo get_category_link(1); ?>">Siirry arkistoon</a></li></ul>
-<?php get_footer(); ?>

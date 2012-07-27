6ab33d35b6dd775f1729befe78177ba2c2fdf433
Ville Vuorenmaa <villevuor@gmail.com>
2012-07-27 12:24:10 +0300

    Kajaus archive

--- functions.php
+++ functions.php
@@ -6,6 +6,7 @@ add_theme_support('post-thumbnails');
 add_image_size('article-big', 580, 250, true);
 add_image_size('article-small', 280, 120, true);
 add_image_size('sidebar-thumb', 160, 100, true);
+add_image_size('kajaus-cover', 150, 215, true);
 
 register_sidebar(array('name' => 'Sivupalkki', 'id' => 'sivupalkki', 'before_widget' => '', 'after_widget' => '', 'before_title' => '<h2>', 'after_title' => '</h2>'));
 register_sidebar(array('name' => 'Yhteystiedot', 'id' => 'contact', 'before_widget' => '<address>', 'after_widget' => '</address>', 'before_title' => '<strong>', 'after_title' => '</strong>'));

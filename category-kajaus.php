a1d1e37ce079f1f0fb1bde9fd3513e1885d8eaeb
Ville Vuorenmaa <villevuor@gmail.com>
2012-07-27 12:38:49 +0300

    Kajaus archive

--- category-kajaus.php
+++ category-kajaus.php
@@ -3,22 +3,22 @@
 <div class="articles">
 	<h2>Kajaus</h2>
 	<p><?php echo category_description($cat->cat_ID); ?></p>
+	<?php if(have_posts()) : ?>
+		<?php while(have_posts()) : ?>
+			<article class="kajaus">
+				<?php the_post(); ?>
+				<a href="<?php the_permalink(); ?>">
+					<?php
+						if (has_post_thumbnail()) {
+							the_post_thumbnail('kajaus-cover', array('class' => 'pull-left'));
+						} 
+					?>
+				</a>
+				<h3><a href="<?php the_permalink(); ?>">Kajaus <?php echo $i--; ?>/2012</a></h3>
+				<p class="meta"><?php the_time("j.n.Y"); ?></p>
+				<p><?php the_content('Lue lehti &rarr;'); ?>
+			</article>
+		<?php endwhile; ?>
+	<?php endif; ?>
 </div>
-<?php if(have_posts()) : ?>
-	<?php while(have_posts()) : ?>
-		<article class="kajaus">
-			<?php the_post(); ?>
-			<a href="<?php the_permalink(); ?>">
-				<?php
-					if (has_post_thumbnail()) {
-						the_post_thumbnail('kajaus-cover', array('class' => 'pull-left'));
-					} 
-				?>
-			</a>
-			<h3><a href="<?php the_permalink(); ?>">Kajaus <?php echo $i--; ?>/2012</a></h3>
-			<p class="meta"><?php the_time("j.n.Y"); ?></p>
-			<p><?php the_content('Lue lehti &rarr;'); ?>
-		</article>
-	<?php endwhile; ?>
-<?php endif; ?>
 <?php get_footer(); ?>

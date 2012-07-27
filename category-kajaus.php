319b938ae494fe391638c6a563711b4177a23672
Ville Vuorenmaa <villevuor@gmail.com>
2012-07-27 12:27:34 +0300

    Kajaus archive

--- category-kajaus.php
+++ category-kajaus.php
@@ -1,20 +1,22 @@
 ﻿<?php get_header(); ?>
 <?php $i = $wp_query->post_count; ?>
-<div class="articles">
+<div class="articles kajaus">
 	<h2>Kajaus</h2>
 	<p><?php echo category_description($cat->cat_ID); ?></p>
 	<?php if(have_posts()) : ?>
 		<?php while(have_posts()) : ?>
 			<div class="clear">
 				<?php the_post(); ?>
-				<?php
-					if (has_post_thumbnail()) {
-						the_post_thumbnail('kajaus-cover', array('class' => 'pull-left'));
-					} 
-				?>
+				<a href="<?php the_permalink(); ?>">
+					<?php
+						if (has_post_thumbnail()) {
+							the_post_thumbnail('kajaus-cover', array('class' => 'pull-left'));
+						} 
+					?>
+				</a>
 				<h3><a href="<?php the_permalink(); ?>">Kajaus <?php echo $i--; ?>/2012</a></h3>
 				<p class="meta"><?php the_time("j.n.Y"); ?></p>
-				<p><?php the_excerpt(); ?>
+				<p><?php the_content('Lue lehti &rarr;'); ?>
 			</div>
 		<?php endwhile; ?>
 	<?php endif; ?>
